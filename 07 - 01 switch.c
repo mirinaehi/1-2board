30530856db0f08f334c64aa652f02ff26ba95f26
mirinaehi <garook@e-mirim.hs.kr>
2021-06-03 13:38:23 +0900

    [문법설명] switch 바꾸기 전

---
+++ 07 - 01 switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int a = 3;
+
+	if (a == 1)
+	{
+		printf("한화의 김성근");
+	}
+	else if (a == 2)
+	{
+		printf("감독님 사랑해");
+	}
+	else
+	{
+		printf("예예예 예예예 예예예 예예예 예예예 예예예 예");
+	}
+}
