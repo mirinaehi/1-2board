13e05a3c1a9f934bd6e420984034f6a7203888c8
mirinaehi <garook@e-mirim.hs.kr>
2021-06-24 15:06:41 +0900

    [문법설명] break문

--- 07 - 03 구구단.c
+++ 07 - 03 구구단.c
@@ -2,10 +2,18 @@
 
 void main(void)
 {
-	for (int dan = 2; dan <= 9; dan++)
-	{		
-		for (int i = 1; i <= 9; i++)
+	for (int dan = 2; dan <= 9; dan++) // 바깥쪽 for문
+	{	
+		if (dan == 4)
 		{
+			break;	// 바깥쪽 for문을 벗어난다
+		}
+		for (int i = 1; i <= 9; i++) // 안쪽 for문
+		{
+			if (i == 5)
+			{
+				break;	// 안쪽 for문을 벗어난다
+			}
 			printf("%d X %d = %d\n", dan, i, dan * i);
 		}
 		printf("\n");
