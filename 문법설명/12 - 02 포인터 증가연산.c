9968a3533d53ab35ee6c5d07c2fc7b2ee6e6ad93
mirinaehi <garook@e-mirim.hs.kr>
2021-10-22 14:58:53 +0900

    포인터에 1씩 증가하면 가리키는(참조하는) 대상의 크기만큼 증가한다.

---
+++ 문법설명/12 - 02 포인터 증가연산.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void main(void)
+{
+	int i = 1;
+	char c = 2;
+	double b = 3.0;
+
+	int* pi = &i;
+	char* pc = &c;
+	double* pb = &b;
+
+	printf("%p %p %p \n", pi, pc, pb);
+	// 포인터에 1을 더할때마다 참조하는 대상의 메모리크기만큼 증가한다
+	// pi는 4씩(int) 증가, pc는 1씩(char), pb는 8씩(double) 증가
+	printf("%p %p %p \n", pi+1, pc+1, pb+1);
+	printf("%p %p %p \n", &i+1, &c+1, &b+1);
+
+	printf("%d %d %lf \n", i, c, b);
+	printf("%d %d %lf \n", i + 1, c + 1, b + 1.0);
+}
