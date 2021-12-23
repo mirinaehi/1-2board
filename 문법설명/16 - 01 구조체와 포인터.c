10202b92818c91588e9e112b3fc8af659f890aec
mirinaehi <garook@e-mirim.hs.kr>
2021-12-23 13:59:00 +0900

    포인터로 구조체를 가리킴

---
+++ 문법설명/16 - 01 구조체와 포인터.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+	int x;
+	int y;
+}Point;
+
+void main(void)
+{
+	// 지역변수 a는 stack영역에 할당된다.
+	Point a = { 3, 4 };
+	printf("%d %d\n", a.x, a.y);
+
+	Point* ptr = &a;
+	// 아래 두 식은 같은 식이다.
+	printf("%d %d\n", (*ptr).x, (*ptr).y);
+	printf("%d %d\n", ptr->x, ptr->y);
+
+
+}
+
